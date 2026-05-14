381c6e85ab0bddeed510df4abe7dc25aa7e12f96
Adityakandregula <adityakandregula@example.com>
2026-05-15 00:21:04 +0530

    Configure root deployment for Vercel by using base / and root router basename

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isVercel = process.env.VERCEL === '1' || process.env.VERCEL === 'true';
-
 export default defineConfig({
-  base: isVercel ? '/' : '/MISSION-6/',
+  base: '/',
   plugins: [react()],
 });
